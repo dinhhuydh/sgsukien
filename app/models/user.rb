7396bf3a826cefee975f94f4ebe322679ed3f9cf
Huy Dinh <dinhhuydh@gmail.com>
2013-03-05 22:13:43 +0700

    [f] User has a role. The role can be  holder, collector, admin or user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,14 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+                  :role
+
+  ROLES = %w[admin holder collector user]
+
+  ROLES.each do |role_name|
+    define_method(role_name.to_s + '?') do
+      role == role_name.to_s
+    end
+  end
 end
